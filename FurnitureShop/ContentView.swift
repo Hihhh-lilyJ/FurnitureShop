4df07f1a366e5376f00dd978b2df9d51e3fd383d
Hihhh-lilyJ <eggylilwin@gmail.com>
2023-03-01 23:37:37 +0630

    Initial Commit

---
+++ FurnitureShop/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FurnitureShop
+//
+//  Created by MacBook Pro on 01/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ FurnitureShop/FurnitureShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureShopApp.swift
+//  FurnitureShop
+//
+//  Created by MacBook Pro on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
